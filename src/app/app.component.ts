5fd5d0462be03b6893476af5dae7cd4a9837e93f
paulQR <paulqr10@gmail.com>
2019-07-17 15:41:10 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,4 +15,18 @@ export class AppComponent  {
   a = 0.234; 
 
   salario = 1234.5;
+
+  heroe = {
+    nombre: "Logan",
+    clave: "Wolverine",
+    edad: 500,
+    direccion: {
+      calle: "Primera",
+      casa: "19"
+    }
+  }
+
+  valorDePromesa = new Promise( (resolve, reject)=>{
+    setTimeout(()=>resolve('Llego la data!'),3500);
+  });
 }
