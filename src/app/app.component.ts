4607a2c422c6e8e4ac695426ef8b7072e73191b9
paulQR <paulqr10@gmail.com>
2019-07-17 15:23:52 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   nombre = "Fernando";
+
+  arreglo = [1,2,3,4,5,6,7,8,9,10];
+
+  PI = Math.PI;
+
+  a = 0.234; 
+
+  salario = 1234.5;
 }
